1e563467367b9681fa6f3bfed84f2465609b0557
scoyne <scoyne@bw.edu>
2018-04-03 20:28:26 -0400

    completed checkpoint address-bloc-csv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,22 +1,11 @@
 require_relative 'entry'
+require "csv"
 
   class AddressBook
     attr_reader :entries
 
     def initialize
       @entries = []
-    end
-    
-    def remove_entry(name, phone_number, email)
-      delete_entry = nil
-      
-      @entries.each do |entry|
-        if name == entry.name && phone_number = entry.phone_number && email == entry.email
-          delete_entry = entry
-        end
-      end
-
-      @entries.delete(delete_entry)
     end
 
     def add_entry(name, phone_number, email)
@@ -29,4 +18,27 @@ require_relative 'entry'
     end
     @entries.insert(index, Entry.new(name, phone_number, email))
    end
+
+   def remove_entry(name, phone_number, email)
+     delete_entry = nil
+
+     @entries.each do |entry|
+       if name == entry.name && phone_number = entry.phone_number && email == entry.email
+         delete_entry = entry
+       end
+     end
+
+     @entries.delete(delete_entry)
+   end
+
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+   end
+   
  end
